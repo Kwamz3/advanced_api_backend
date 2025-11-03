88d96bb88898e3c71cc0a097d9d2143b9ba20fb1
Kwamz3 <charlie18appiah@gmail.com>
2025-11-03 16:38:43 +0000

    Implement user authentication verification logic in users.py

--- backend-dev1/app/api/v1/users.py
+++ backend-dev1/app/api/v1/users.py
@@ -11,3 +11,27 @@ from app.core.security import verify_token
 router = APIRouter()
 security = HTTPBearer() 
 
+async def verify_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    
+    token = credentials.credentials
+    
+    try:
+        payload = verify_token(token)
+        user_id = payload.get("sub")
+        role = payload.get("role")
+        
+        if user_id is None:
+            raise HTTPException(
+                status_code= status.HTTP_401_UNAUTHORIZED,
+                detail= "Invalid authenttication token: missing subject (sub)",
+                headers= {"WWW-Authentication": "Bearer"}
+            )
+        
+        return {"user": user_id, "role": role}
+    
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail= "Could not validate credentials",
+            headers= {"WWW-Authentication": "Bearer"}
+        )
