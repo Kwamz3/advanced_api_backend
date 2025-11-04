06cd420a64c3cb66ea13865804e7c490d1dc8904
Kwamz3 <charlie18appiah@gmail.com>
2025-11-04 20:31:08 +0000

    Add lifespan management to FastAPI app with logging for startup and shutdown

--- backend-dev1/main.py
+++ backend-dev1/main.py
@@ -4,6 +4,7 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from contextlib import asynccontextmanager
 from typing import Optional
 from dotenv import load_dotenv
+import logging
 import uvicorn
 import os
 load_dotenv()
@@ -14,4 +15,24 @@ from app.core.database import init_db
 from app.core.security import verify_token
 
 security = HTTPBearer()
+logger = logging.getLogger(__name__)
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Starting Streamplus backend...")
+    
+    await init_db()
+    logger.info("Database initialized...")
+    
+    yield
+    logger.info("Shutting down Streamplus Backend...")
+    
+    
+app = FastAPI(
+    title= "Streamplus API",
+    description= "Backend API for Streamplus - Movie streaming platform",
+    version= "1.0.0",
+    docs_url= "/docs",
+    redoc_url= "/redoc",
+    lifespan= lifespan
+)
