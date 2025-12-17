2d7887b36c8070e291bafd76605e9c047d4f49d2
Kwamz3 <charlie18appiah@gmail.com>
2025-12-17 06:29:05 +0000

    Remove unused import 'types' from init_db function in database.py

--- backend-dev1/app/core/database.py
+++ backend-dev1/app/core/database.py
@@ -71,7 +71,7 @@ async def get_db():
 async def init_db():
     """Initialize database tabels"""
     try:
-        from app.models import admin, category, movies, series, types, users
+        from app.models import admin, category, movies, series, users
         
         async with engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
